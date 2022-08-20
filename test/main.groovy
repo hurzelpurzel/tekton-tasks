92c4eb22b8c1ed74da7228ddf8dd44964c740845
ludger.pottmeier@atos.net <ludger.pottmeier@atos.net>
2022-08-20 20:05:48 +0200

    test with output

--- test/main.groovy
+++ test/main.groovy
@@ -1,1 +1,27 @@
+import groovy.yaml.YamlBuilder;
+import groovy.yaml.YamlSlurper;
+
+
+def workspace = System.getenv('WORKSPACE_SOURCE')
+
+
+def loadInput(filename){
+  def ys = new YamlSlurper()
+  File fh1 = new File(workspace+"/"+ filename)
+  text = fh1.getText('UTF-8')
+  return ys.parseText(text);
+}
+
+def writeOutput(filename, output){
+   def builder = new YamlBuilder()
+   builder.out = output
+   File file = new File(workspace+"/"+ filename)
+   file.write builder.toString()
+}
+
+def output.test = "Testausgabe"
+
+
 println 'This is the Groovy world!'
+
+writeOutput( "out.yaml" , output)
